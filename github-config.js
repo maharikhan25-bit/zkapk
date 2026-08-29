1c54e9163b2b5ad442a1f23b517e143e0bbd6e19
maharikhan25-bit <maharikhan25@gmail.com>
2026-08-29 08:08:16 +0430

    Create github-config.js

---
+++ github-config.js
@@ -0,0 +1,7 @@
+// د GitHub تنظیمات — دا معلومات خوندي دي (پټ ندي)
+const GITHUB_OWNER = "maharikhan25-bit";
+const GITHUB_REPO = "zkapk";
+const GITHUB_BRANCH = "main";
+
+// دا پاسورډ یوازې تاسو ته اجازه درکړي چې د اپلوډ پینل خلاص کړئ
+const UPLOAD_PASSWORD = "edris_zirak";
